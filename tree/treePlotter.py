a6a23cb58e557c4db484e78d2fb9f003bf8648e0
chuiyuan <lisjthu@sina.com>
2016-08-14 17:47:16 +0800

    decision tree ID3

---
+++ tree/treePlotter.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+
+decisionNode = dict(boxstyle='sawtooth', fc='0.8')
+leafNode = dict(boxstyle='round4', fc = '0.8')
+arrow_args = dict(arrowstyle='<-')
+
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
+                            xytext=centerPt, textcoords='axes fraction',
+                            va ='center', ha='center', bbox=nodeType, arrowprops=arrow_args)
+
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor= 'white')
+    fig.clf()
+    axprops = dict(xticks=[],yticks = [])
+    createPlot.ax1 = plt.subplot(111,frameon=False, **axprops)
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW;
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5,1.0), '')
+    plt.show()
+    # createPlot.ax1 = plt.subplot(111,frameon = False)
+    # plotNode(U'decision point',(0.5,0.1), (0.1,0.5), decisionNode)
+    # plotNode(U'leaf node',(0.8,0.1),(0.3,0.8), leafNode)
+    # plt.show()
+
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = list(myTree)[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__== 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = list(myTree)[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1+ getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth=thisDepth
+    return maxDepth
+
+
+def plotMidText(cntrPt, parentPt, txtStr):
+    xMid = (parentPt[0]-cntrPt[0])/2+cntrPt[0]
+    yMid = (parentPt[1]-cntrPt[1])/2 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtStr)
+
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree)[0]
+    cntrPt = (plotTree.xOff+(1.0+float(numLeafs))/2/plotTree.totalW,
+              plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode(secondDict[key],(plotTree.xOff, plotTree.yOff),
+                     cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff),cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+
+#print(getTreeDepth({'l1': {0: 'no', 1: {'l2': {0: 'no', 1: 'yes'}}}}))
+
+#createPlot({'l1': {0: 'no', 1: {'l2': {0: 'no', 1: 'yes'}}}})
+createPlot({'tearRate': {'normal': {'astigmatic': {'no': {'age': {'pre': 'soft', 'presbyopic': {'prescript': {'myope': 'no lenses', 'hyper': 'soft'}}, 'young': 'soft'}}, 'yes': {'prescript': {'myope': 'hard', 'hyper': {'age': {'pre': 'no lenses', 'presbyopic': 'no lenses', 'young': 'hard'}}}}}}, 'reduced': 'no lenses'}}
+)
