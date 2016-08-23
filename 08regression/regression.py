897513234f71e9379cd425907b5f1b5d8fcf181c
chuiyuan <lisjthu@sina.com>
2016-08-23 16:11:04 +0800

    local weighted linear regression

--- 08regression/regression.py
+++ 08regression/regression.py
@@ -26,25 +26,50 @@ def standRegres(xArr, yArr):
     return ws
 
 
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    m = shape(xMat)[0]
+    weights = mat(eye(m))
+    for j in range(m):
+        diffMat = testPoint - xMat[j,:]
+        weights[j,j] = exp(diffMat*diffMat.T/(-2.0*k**2))
+    xTx = xMat.T*(weights*xMat)
+    if linalg.det(xTx) == 0.0:
+        print('this matrix is singular, can not do inverse')
+        return
+    ws = xTx.I*(xMat.T*(weights*yMat))
+    return testPoint*ws
+
+
+def lwlrTest(testArr, xArr, yArr, k =1.0):
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i],xArr, yArr,k)
+    return yHat
+
+
 xArr, yArr = loadDataSet('./ex0.txt')
 ws = standRegres(xArr, yArr)
 print(ws)
+yHat = lwlrTest(xArr, xArr, yArr, 0.02)
 
 xMat = mat(xArr)
 xCopy = xMat.copy()
 yMat = mat(yArr)
-yHat = xMat*ws
+# yHat = xMat*ws
 import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-ax.scatter(xMat[:,1].flatten(), yMat.T[:,0].flatten())
+# ax.scatter(xMat[:,1].flatten(), yMat.T[:,0].flatten())
 
-#xCopy.sort(0)
-print(xCopy)
-xHat = xCopy*ws
-ax.plot(xCopy[:,1], yHat)
+
+
+srcIndex = xMat[:,1].argsort(0)
+xSorted = xMat[srcIndex][:,0,:]
+ax.scatter(xMat[:,1], yHat)
 plt.show()
-
 
 
 
